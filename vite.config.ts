0f3613a273240c66b34c5b114a933a671c083c14
rahmad0311 <rahmad193310@gmail.com>
2025-06-09 11:56:52 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rahmad0311/',
+  base: '/rahmad0311/rahmad0311/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
